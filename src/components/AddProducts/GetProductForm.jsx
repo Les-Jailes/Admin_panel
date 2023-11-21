f75a1c6212b72f542f0509c7408ba30bd9c22007
Carlos Antonio Ballester Panigua <carlos.ballester.10387210@usalesiana.edu.bo>
2023-11-21 10:49:24 -0400

    Added a better structure for reading db products.

--- src/components/AddProducts/GetProductForm.jsx
+++ src/components/AddProducts/GetProductForm.jsx
@@ -1,51 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import API from '@/components/Api/api';
 import "@/css/AddProducts/GetProductForm.css";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { TbEditCircle } from "react-icons/tb";
 
 const GetProductForm = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await API.get("/Product");
+        console.log("Response:", response.data);
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Axios Error:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className='container'>
-      <table>
-        <thead>
-          <tr>
-            <th>Code</th>
-            <th>Name of Product</th>
-            <th>Price</th>
-            <th>Category</th>
-            <th>Type</th>
-            <th>Color</th>
-            <th>Size</th>
-            <th>Image</th>
-            <th>Action</th>
-          </tr>
-        </thead>
+      <div>
+        <button className='add-button'>
+          Add Product
+        </button>
+      </div>
+      <div className='table-container'>
+        <table>
+          <thead>
+            <tr>
+              <th>Code</th>
+              <th>Name of Product</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Type</th>
+              <th>Color</th>
+              <th>Sizes</th>
+              <th>Image</th>
+              <th>Action</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-            <td>001</td>
-            <td>Product 1</td>
-            <td>$19.99</td>
-            <td>Women</td>
-            <td>Shirts</td>
-            <td>Blue</td>
-            <td>L</td>
-            <td><img src="product_image_url" alt="Product" /></td>
-            <td><button>Edit</button><button>Delete</button></td>
-          </tr>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product._id.$oid}>
+                <td className='code'>{product.code}</td>
+                <td>{product.name}</td>
+                <td className='price'>${product.price}</td>
+                <td className='category'>{product.category}</td>
+                <td className='type'>{product.type}</td>
+                <td className='color'>{product.color?.join(', ')}</td>
+                <td className='size'>
+                  {product.sizes.map((size, index) => (
+                    <div key={index}>
+                      {`${size.size}: ${size.quantity}`}
+                    </div>
+                  ))}
+                </td>
 
-          <tr>
-            <td>002</td>
-            <td>Product 2</td>
-            <td>$21.00</td>
-            <td>Men</td>
-            <td>Pants</td>
-            <td>Black</td>
-            <td>M</td>
-            <td><img src="product_image_url" alt="Product" /></td>
-            <td><button>Edit</button><button>Delete</button></td>
-          </tr>
-          
-        </tbody>
-      </table>
+                <td className='image'>
+                  {product.path.length > 0 && (
+                    <img src={product.path[0]} alt={`Product 1`} style={{ width: '50px', height: '50px' }} />
+                  )}
+                </td>
+
+                <td>
+                  <IoMdCloseCircleOutline className='delete-button' />
+                  <TbEditCircle className='edit-button' />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
