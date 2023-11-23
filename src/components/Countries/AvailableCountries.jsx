53439e6c4420ce288b26bc5c0c3f9ecac752d164
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2023-11-23 00:42:37 -0400

    Missing button added

--- src/components/Countries/AvailableCountries.jsx
+++ src/components/Countries/AvailableCountries.jsx
@@ -1,10 +1,21 @@
+"use client";
 import React from 'react';
 import '@/css/Countries/AvailableCountries.css'; 
 
 const AvailableCountries = ({ countries }) => {
+    const addNewProduct = () => {
+        window.location.href = '/countries/country-form';
+    }
+
     return (
       <div className="countries-container">
-        <h2>Available Countries</h2>
+        <div className="header-container">
+          <h2>Available Countries</h2>
+          <div className="header-buttons">
+            <button className="delete-country-btn">Delete country</button>
+            <button type='button' className="add-country-btn" onClick={addNewProduct} >Add new country</button>
+          </div>
+        </div>
         <div className="countries-grid">
           {countries.map((country, index) => (
             <div key={index} className="country-card">
@@ -23,6 +34,6 @@ const AvailableCountries = ({ countries }) => {
         </div>
       </div>
     );
-  };
+};
 
 export default AvailableCountries;
